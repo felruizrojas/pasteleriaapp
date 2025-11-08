c7b33b2543f221d679add823831bae95a138c76b
Felipe Ruiz Rojas <felruizrojas@Felipes-MacBook-Air-2.local>
2025-11-08 16:42:56 -0300

    Desarrollo de CategoriaDao y ProductoDao [data/local/dao]

--- app/src/main/java/com/example/pasteleriaapp/data/local/dao/CategoriaDao.kt
+++ app/src/main/java/com/example/pasteleriaapp/data/local/dao/CategoriaDao.kt
@@ -1,2 +1,35 @@
 package com.example.pasteleriaapp.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pasteleriaapp.data.local.entity.CategoriaEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface CategoriaDao {
+    @Query("SELECT * FROM categoria ORDER BY nombreCategoria ASC")
+    fun obtenerCategorias(): Flow<List<CategoriaEntity>>
+
+    @Query("SELECT * FROM categoria WHERE idCategoria = :idCategoria")
+    suspend fun obtenerCategoriaPorId(idCategoria: Int): CategoriaEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarCategoria(categoria: CategoriaEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarCategorias(categorias: List<CategoriaEntity>)
+
+    @Update
+    suspend fun actualizarCategoria(categoria: CategoriaEntity)
+
+    @Delete
+    suspend fun eliminarCategoria(categoria: CategoriaEntity)
+
+    @Query("DELETE FROM categoria")
+    suspend fun eliminarTodasLasCategorias()
+}
--- app/src/main/java/com/example/pasteleriaapp/data/local/dao/ProductoDao.kt
+++ app/src/main/java/com/example/pasteleriaapp/data/local/dao/ProductoDao.kt
@@ -1,2 +1,34 @@
 package com.example.pasteleriaapp.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pasteleriaapp.data.local.entity.ProductoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductoDao {
+    @Query("SELECT * FROM producto ORDER BY nombreProducto ASC")
+    fun obtenerProductos(): Flow<List<ProductoEntity>>
+
+    @Query("SELECT * FROM producto WHERE idProducto = :idProducto")
+    suspend fun obtenerProductoPorId(idProducto: Int): ProductoEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarProducto(producto: ProductoEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarProductos(productos: List<ProductoEntity>)
+
+    @Update
+    suspend fun actualizarProducto(producto: ProductoEntity)
+
+    @Delete
+    suspend fun eliminarProducto(producto: ProductoEntity)
+
+    @Delete
+    suspend fun eliminarProductos(productos: List<ProductoEntity>)
+}
